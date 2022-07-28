f91a94e7346c4b887d879a13d1b117f8a1484045
qtgiebel <qtgiebel@gmail.com>
2022-07-28 10:25:32 -0500

    Update form to accept input file and trigger analyze action

--- Controllers/AnalyzerController.cs
+++ Controllers/AnalyzerController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using analyzer_refactor.Models;
 
@@ -8,16 +9,59 @@ public class AnalyzerController : Controller
 {
 
     private readonly ILogger<AnalyzerController> _logger;
+    private readonly string _fileFormat = ".txt";
+
+    private List<ITokenAnalyzer> analyzers;
 
     public AnalyzerController(ILogger<AnalyzerController> logger)
     {
         _logger = logger;
-        _logger.LogInformation("Initialized AnalyzerController");
+        analyzers = new List<ITokenAnalyzer> {
+            new DistinctTokenAnalyzer()
+        };
+
     }
     public IActionResult Index()
     {
-        _logger.LogInformation("Inside Analyzer/Index action");
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Analyze(IFormFile file)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Error();
+        }
+
+        if (System.IO.Path.GetExtension(file.FileName).Substring(1) != _fileFormat)
+        {
+            return Error();
+        }
+
+        Stream fileStream = file.OpenReadStream();
+        List<string> tokens = new List<string>();
+        string pattern = "\\W";
+        Regex rgx = new Regex(pattern);
+
+        using (StreamReader reader = new StreamReader(fileStream))
+        {
+            string line = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] splitLine = rgx.Split(line);
+                foreach (var word in splitLine)
+                {
+                    if (!String.IsNullOrWhiteSpace(word) && !String.IsNullOrEmpty(word))
+                    {
+                        tokens.Add(word);
+                    }
+                }
+            }
+            reader.Close();
+        }
+
+        return View(new InputFileModel());
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/InputFileModel.cs
@@ -0,0 +1,6 @@
+namespace analyzer_refactor.Models;
+
+public class InputFileModel
+{
+    public IFormFile? uploadFile { set; get; }
+}

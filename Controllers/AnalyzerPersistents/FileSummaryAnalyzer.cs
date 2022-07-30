679c7fe6c61a0162457ba5335b95516c82802311
qtgiebel <qtgiebel@gmail.com>
2022-07-30 17:04:41 -0500

    create file summary analyzer

---
+++ Controllers/AnalyzerPersistents/FileSummaryAnalyzer.cs
@@ -0,0 +1,34 @@
+/**
+    Analyzes the tokens inputed to determine which of them are distinct.
+*/
+class FileSummaryAnalyzer : ITokenAnalyzer
+{
+    private int tokenCount { get; set; }
+
+    /* 
+        Initializes an empty list for the distinct tokens. 
+        The set will always be initialized empty and be populated while running.
+    */
+    public FileSummaryAnalyzer() => tokenCount = 0;
+
+    /* 
+        Adds the current token to the list if it is not already present.
+    */
+    public void process(string token) => tokenCount++;
+
+    /*
+        Generate the output file.
+    */
+    public void generateOutputFile(string filePath)
+    {
+
+    }
+
+    /* 
+        Formats and prints the list of distinct tokens.
+    */
+    public void printToFile()
+    {
+
+    }
+}
